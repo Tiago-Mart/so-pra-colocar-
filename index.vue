8dd087583c82f23b0490b5338ccbfca1d95b0e05
Tiago Martins <82526424+Tiago-Mart@users.noreply.github.com>
2022-08-30 14:05:15 -0300

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,97 @@
+
+
+<template>
+  <!--
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700&family=Spectral:ital,wght@0,400;0,600;0,700;1,300;1,400&display=swap" rel="stylesheet">
+  -->
+
+  <section class="body" @click="esconder()">
+    <TopBar></TopBar>
+    <MobileNav></MobileNav>
+    <PrimaryMenu></PrimaryMenu>
+    <section class="geral">
+      <section class="home">
+        <section class="init">
+          <h1>Explore novos temas.</h1>
+          <p>Centenas de artigos te esperam</p>
+          <form action="">
+            <input
+              type="search"
+              name="search"
+              id="search"
+              placeholder="Descubra algo novo..."
+            />
+            <button type="submit" id="search-submit"></button>
+          </form>
+        </section>
+
+        <div class="img"></div>
+      </section>
+
+      <section class="articles">
+        <section class="article day">
+          <h2>Artigo do Dia</h2>
+          <Card :random="false"></Card>
+        </section>
+
+        <section class="article day">
+          <h2>Explorar Artigos</h2>
+          <Card></Card>
+          <Card></Card>
+        </section>
+
+        <button v-on:click = "teste"> TESTE </button>
+      </section>
+    </section>
+  </section>
+</template>
+
+
+<style>
+</style>
+
+<script>
+export default {
+  name: "IndexPage",
+  head() {
+    return {
+      title: "Explore novos temas.",
+    };
+  },
+  methods: {
+    menuBTn() {
+      const btn = document.querySelector("button.menu-button");
+      btn.classList.contains("clicado")
+        ? btn.classList.remove("clicado")
+        : btn.classList.add("clicado");
+    },
+
+    // esconder o menu quando clicar fora dele
+    esconder() {
+      const primaryNav = document.querySelector(
+        "section.search-results-dropdown"
+      );
+      var visi = primaryNav.getAttribute("data-visible");
+      visi == "true"
+        ? primaryNav.setAttribute("data-visible", "false")
+        : primaryNav.setAttribute("data-visible", "true");
+    },
+
+    teste(){
+      this.$axios.$get("/teste").then(function(response){
+        data = response.data;
+      })
+    }
+
+  },  
+  components: { TopBar, MobileNav, Card, PrimaryMenu },
+};
+
+import TopBar from "~/components/TopBar.vue";
+import MobileNav from "~/components/MobileNav.vue";
+import Card from "~/components/Card.vue";
+import PrimaryMenu from "~/components/PrimaryMenu.vue";
+</script>
+
---
+++ nuxt.config.js
@@ -0,0 +1,78 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'server',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Cronopedia',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/static/favicon.ico' },
+
+      // <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+      { rel: 'stylesheet', type: 'text/css', href: "https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" },
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    "~/static/css/style.css"
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: 'http://localhost:8080/',
+  },
+
+  serverMiddleware: ["~/server-middleware/index"],
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    meta: {
+      title: 'Cronopédia',
+    },
+    manifest: {
+      name: "Cronopédia",
+      short_name: "Cronopédia",
+      author: "Equipe Cronopédia",
+      description: "Uma forma mais uma fácil de aprender.",
+      lang: "pt-BR",
+      themeColor: "#000000",
+    },
+    icon: {
+      fileName: 'icon.png',
+      sizes: [128, 192, 256, 512],
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
